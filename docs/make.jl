e536aa439fb5a5f2325f3c37e4119825ecac8799
cboennec <boennec@laplace.univ-tlse.fr>
2023-04-11 10:55:27 +0200

    try to add documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,10 @@
 using Pkg
 
+ENV["PYTHON"] = ""
+Pkg.build("PyCall")
+using Conda
+Conda.add("numpy"; channel="conda-forge")
+
 Pkg.activate(".")
 
 Pkg.add(url="https://github.com/hradet/Metaheuristics.jl")
