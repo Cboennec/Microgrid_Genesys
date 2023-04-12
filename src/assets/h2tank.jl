0a19c1a4fc271f25ab9fba5a4ec09bcce0fa8e8c
cboennec <boennec@laplace.univ-tlse.fr>
2023-04-12 15:02:36 +0200

    Try to make the same environment for doc building

--- src/assets/h2tank.jl
+++ src/assets/h2tank.jl
@@ -2,6 +2,29 @@
     H2 tank storage modelling
  =#
 
+"""
+H2Tank
+
+A mutable struct representing a hydrogen tank storage model with various parameters, initial conditions, variables, and an inner constructor.
+
+# Fields
+- α_p_ch::Float64: Maximum charging power factor (default: 1.5)
+- α_p_dch::Float64: Maximum discharging power factor (default: 1.5)
+- η_ch::Float64: Charging efficiency (default: 1.0)
+- η_dch::Float64: Discharging efficiency (default: 1.0)
+- η_self::Float64: Self-discharge rate (default: 0.0)
+- α_soc_min::Float64: Minimum state of charge factor (default: 0.0)
+- α_soc_max::Float64: Maximum state of charge factor (default: 1.0)
+- lifetime::Int64: Storage lifetime in years (default: 25)
+- bounds::NamedTuple{(:lb, :ub), Tuple{Float64, Float64}}: Lower and upper bounds of storage capacity (default: (lb = 0., ub = 10000.))
+- Erated_ini::Float64: Initial rated storage energy capacity (default: 1e-6)
+- soc_ini::Float64: Initial state of charge (default: 0.5)
+- soh_ini::Float64: Initial state of health (default: 1.0)
+- Erated::AbstractArray{Float64,2}: Rated storage energy capacity over time and scenarios
+- carrier::Hydrogen: Hydrogen carrier for the H2 tank
+- soc::AbstractArray{Float64,3}: State of charge over time, years, and scenarios
+- cost::AbstractArray{Float64,2}: Cost of the hydrogen storage over time and scenarios
+"""
 mutable struct H2Tank  <: AbstractStorage
      # Paramètres
      α_p_ch::Float64
@@ -39,7 +62,11 @@ mutable struct H2Tank  <: AbstractStorage
         new(α_p_ch, α_p_dch, η_ch, η_dch, η_self, α_soc_min, α_soc_max, lifetime, bounds, Erated_ini, soc_ini, soh_ini)
 end
 
-### Preallocation
+"""
+    preallocate!(h2tank::H2Tank, nh::Int64, ny::Int64, ns::Int64)
+
+Preallocate memory for the `H2Tank` object with given dimensions for hours (nh), years (ny), and scenarios (ns).
+"""
 function preallocate!(h2tank::H2Tank, nh::Int64, ny::Int64, ns::Int64)
     h2tank.Erated = convert(SharedArray,zeros(ny+1, ns)) ; h2tank.Erated[1,:] .= h2tank.Erated_ini
     h2tank.carrier = Hydrogen()
@@ -49,11 +76,20 @@ function preallocate!(h2tank::H2Tank, nh::Int64, ny::Int64, ns::Int64)
     return h2tank
 end
 
-### Operation dynamic
+"""
+    compute_operation_dynamics!(h::Int64, y::Int64, s::Int64, h2tank::H2Tank, decision::Float64, Δh::Int64)
+
+Compute the operation dynamics of the hydrogen tank storage for a given hour (h), year (y), and scenario (s), using the provided decision and time step (Δh).
+"""
 function compute_operation_dynamics!(h::Int64, y::Int64, s::Int64, h2tank::H2Tank, decision::Float64, Δh::Int64)
      h2tank.soc[h+1,y,s], h2tank.carrier.power[h,y,s] = compute_operation_dynamics(h2tank, (Erated = h2tank.Erated[y,s], soc = h2tank.soc[h,y,s]), decision, Δh)
 end
 
+"""
+    compute_operation_dynamics(h2tank::H2Tank, state::NamedTuple{(:Erated, :soc), Tuple{Float64, Float64}}, decision::Float64, Δh::Int64)
+
+Compute the operation dynamics of the hydrogen tank storage for the given state, decision, and time step (Δh).
+"""
 function compute_operation_dynamics(h2tank::H2Tank, state::NamedTuple{(:Erated, :soc), Tuple{Float64, Float64}}, decision::Float64, Δh::Int64)
      # Control power constraint and correction
      power_dch = max(min(decision, h2tank.α_p_dch * state.Erated, h2tank.η_dch * (state.soc * (1. - h2tank.η_self * Δh) - h2tank.α_soc_min) * state.Erated / Δh), 0.)
@@ -63,11 +99,20 @@ function compute_operation_dynamics(h2tank::H2Tank, state::NamedTuple{(:Erated, 
      return soc_next, power_dch + power_ch
 end
 
-### Investment dynamic
+"""
+    compute_investment_dynamics!(y::Int64, s::Int64, h2tank::H2Tank, decision::Union{Float64, Int64})
+
+Compute the investment dynamics of the hydrogen tank storage for a given year (y) and scenario (s), using the provided decision.
+"""
 function compute_investment_dynamics!(y::Int64, s::Int64, h2tank::H2Tank, decision::Union{Float64, Int64})
     h2tank.Erated[y+1,s], h2tank.soc[1,y+1,s] = compute_investment_dynamics(h2tank, (Erated = h2tank.Erated[y,s], soc = h2tank.soc[end,y,s]), decision)
 end
 
+"""
+compute_investment_dynamics(h2tank::H2Tank, state::NamedTuple{(:Erated, :soc), Tuple{Float64, Float64}}, decision::Union{Float64, Int64})
+
+Compute the investment dynamics of the hydrogen tank storage for the given state and decision.
+"""
 function compute_investment_dynamics(h2tank::H2Tank, state::NamedTuple{(:Erated, :soc), Tuple{Float64, Float64}}, decision::Union{Float64, Int64})
     if decision > 1e-2
         Erated_next = decision
--- src/assets/tes.jl
+++ src/assets/tes.jl
@@ -1,7 +1,29 @@
 #=
     Thermal energy storage modelling
  =#
+ """
+ ThermalStorage
 
+A mutable struct representing a thermal energy storage model with various parameters, initial conditions, variables, and an inner constructor.
+
+# Fields
+- `α_p_ch`::Float64: Maximum charging power factor
+- `α_p_dch`::Float64: Maximum discharging power factor
+- `η_ch`::Float64: Charging efficiency
+- `η_dch`::Float64: Discharging efficiency
+- `η_self`::Float64: Self-discharge rate
+- `α_soc_min`::Float64: Minimum state of charge factor
+- `α_soc_max`::Float64: Maximum state of charge factor
+- `lifetime`::Int64: Storage lifetime in years
+- `bounds`::NamedTuple{(:lb, :ub), Tuple{Float64, Float64}}: Lower and upper bounds of storage capacity
+- `Erated_ini`::Float64: Initial rated storage energy capacity
+- `soc_ini`::Float64: Initial state of charge
+- `soh_ini`::Float64: Initial state of health
+- `Erated`::AbstractArray{Float64,2}: Rated storage energy capacity over time and scenarios
+- `carrier`::Heat: Heat carrier for the thermal storage
+- `soc`::AbstractArray{Float64,3}: State of charge over time, years, and scenarios
+- `cost`::AbstractArray{Float64,2}: Cost of the thermal storage over time and scenarios
+"""
 mutable struct ThermalStorage <: AbstractStorage
      # Paramètres
      α_p_ch::Float64
@@ -39,7 +61,11 @@ mutable struct ThermalStorage <: AbstractStorage
           new(α_p_ch, α_p_dch, η_ch, η_dch, η_self, α_soc_min, α_soc_max, lifetime, bounds, Erated_ini, soc_ini, soh_ini)
 end
 
-### Preallocation
+"""
+    preallocate!(tes::ThermalStorage, nh::Int64, ny::Int64, ns::Int64)
+
+Preallocate memory for the `ThermalStorage` object with given dimensions for hours (nh), years (ny), and scenarios (ns).
+"""
 function preallocate!(tes::ThermalStorage, nh::Int64, ny::Int64, ns::Int64)
     tes.Erated = convert(SharedArray,zeros(ny+1, ns)) ; tes.Erated[1,:] .= tes.Erated_ini
     tes.carrier = Heat()
@@ -49,7 +75,11 @@ function preallocate!(tes::ThermalStorage, nh::Int64, ny::Int64, ns::Int64)
     return tes
 end
 
-### Operation dynamic
+"""
+    compute_operation_dynamics!(h::Int64, y::Int64, s::Int64, tes::ThermalStorage, decision::Float64, Δh::Int64)
+
+Compute the operation dynamics of the thermal energy storage for a given hour (h), year (y), and scenario (s), using the given decision variable and time step (Δh).
+"""
 function compute_operation_dynamics!(h::Int64, y::Int64, s::Int64, tes::ThermalStorage, decision::Float64, Δh::Int64)
      tes.soc[h+1,y,s], tes.carrier.power[h,y,s] = compute_operation_dynamics(tes, (Erated = tes.Erated[y,s], soc = tes.soc[h,y,s]), decision, Δh)
 end
@@ -63,11 +93,22 @@ function compute_operation_dynamics(tes::ThermalStorage, state::NamedTuple{(:Era
      return soc_next, power_dch + power_ch
 end
 
-### Investment dynamic
+
+"""
+    compute_investment_dynamics!(y::Int64, s::Int64, tes::ThermalStorage, decision::Union{Float64, Int64})
+
+Compute the investment dynamics of the thermal energy storage for a given year (y) and scenario (s), using the given decision variable.
+"""
 function compute_investment_dynamics!(y::Int64, s::Int64, tes::ThermalStorage, decision::Union{Float64, Int64})
     tes.Erated[y+1,s], tes.soc[1,y+1,s] = compute_investment_dynamics(tes, (Erated = tes.Erated[y,s], soc = tes.soc[end,y,s]), decision)
 end
 
+
+"""
+    compute_operation_dynamics(tes::ThermalStorage, state::NamedTuple{(:Erated, :soc), Tuple{Float64, Float64}}, decision::Float64, Δh::Int64)
+
+Compute the operation dynamics of the thermal energy storage, given the current state (Erated and soc), decision variable, and time step (Δh).
+"""
 function compute_investment_dynamics(tes::ThermalStorage, state::NamedTuple{(:Erated, :soc), Tuple{Float64, Float64}}, decision::Union{Float64, Int64})
     if decision > 1e-2
         Erated_next = decision
