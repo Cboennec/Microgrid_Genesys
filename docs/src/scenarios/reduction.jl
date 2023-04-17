454f42a82d4d98291d062ddde364819e0c7644c0
cboennec <boennec@laplace.univ-tlse.fr>
2023-04-17 11:19:31 +0200

    scenario comment and draft page

--- docs/src/scenarios/reduction.jl
+++ docs/src/scenarios/reduction.jl
@@ -5,7 +5,24 @@ abstract type AbstractScenariosReducer end
 abstract type AbstractDimensionReducer end
 abstract type AbstractClusteringMethod end
 
-# Manual scenario reduction
+"""
+    mutable struct ManualReducer <: AbstractScenariosReducer
+        h::Union{UnitRange{Int64}, Int64}
+        y::Union{UnitRange{Int64}, Int64}
+        s::Union{UnitRange{Int64}, Int64}
+    end
+
+A mutable structure representing a manual scenario reduction. Inherits from the `AbstractScenariosReducer` abstract type.
+
+# Fields
+- `h::Union{UnitRange{Int64}, Int64}`: The hours to be included in the reduced scenario.
+- `y::Union{UnitRange{Int64}, Int64}`: The years to be included in the reduced scenario.
+- `s::Union{UnitRange{Int64}, Int64}`: The scenarios to be included in the reduced scenario.
+
+# Constructor
+
+    ManualReducer(; h = 1:8760, y = 2:3, s = 1:1) = new(h, y, s)
+"""
 mutable struct ManualReducer <: AbstractScenariosReducer
     h::Union{UnitRange{Int64}, Int64}
     y::Union{UnitRange{Int64}, Int64}
@@ -13,6 +30,27 @@ mutable struct ManualReducer <: AbstractScenariosReducer
     ManualReducer(; h = 1:8760, y = 2:3, s = 1:1) = new(h, y, s)
 end
 
+
+"""
+    function reduce(reducer::ManualReducer, ω::Scenarios)
+
+Reduces the given `Scenarios` object using the specified `ManualReducer`.
+
+# Arguments
+- `reducer::ManualReducer`: The reducer used to specify the reduction process.
+- `ω::Scenarios`: The `Scenarios` object to be reduced.
+
+# Returns
+- A new reduced `Scenarios` object.
+- A probability vector representing the likelihood of each scenario.
+
+# Example
+
+```julia
+manual_reducer = ManualReducer(h = 1:8760, y = 2:3, s = 1:1)
+reduced_scenarios, probabilities = reduce(manual_reducer, scenarios)
+```
+"""
 function reduce(reducer::ManualReducer, ω::Scenarios)
     # Parameters
     h, y, s = reducer.h, reducer.y, reducer.s
@@ -42,13 +80,50 @@ function reduce(reducer::ManualReducer, ω::Scenarios)
     return Scenarios(demands, generations, storages, converters, grids), ones(length(s)) / length(s)
 end
 
-# Sample Average Approximation scenario reduction
+"""
+    mutable struct SAAReducer <: AbstractScenariosReducer
+        nsample::Int64
+    end
+
+A mutable structure representing a Sample Average Approximation (SAA) scenario reduction. Inherits from the `AbstractScenariosReducer` abstract type.
+
+# Fields
+- `nsample::Int64`: The number of samples to be included in the reduced scenario.
+
+# Constructor
+
+    SAAReducer(; nsample = 100) = new(nsample)
+"""
 mutable struct SAAReducer <: AbstractScenariosReducer
     nsample::Int64
 
     SAAReducer(; nsample = 100) = new(nsample)
 end
 
+"""
+    function reduce(reducer::SAAReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
+
+Reduces the given `Scenarios` object using the specified `SAAReducer`, implementing the Sample Average Approximation scenario reduction method.
+
+# Arguments
+- `reducer::SAAReducer`: The reducer used to specify the reduction process.
+- `ω::Scenarios`: The `Scenarios` object to be reduced.
+
+# Keyword Arguments
+- `y::Int64 = 1`: The initial year to be considered.
+- `s::Int64 = 1`: The initial scenario to be considered.
+
+# Returns
+- A new reduced `Scenarios` object.
+- A probability vector representing the likelihood of each scenario.
+
+# Example
+
+```julia
+saa_reducer = SAAReducer(nsample = 100)
+reduced_scenarios, probabilities = reduce(saa_reducer, scenarios)
+```
+"""
 function reduce(reducer::SAAReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
     # Parameters
     _, ny, ns = size(ω.demands[1].power)
@@ -80,11 +155,45 @@ function reduce(reducer::SAAReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
     return Scenarios(demands, generations, storages, converters, grids), ones(reducer.nsample) / reducer.nsample
 end
 
-# Expected value scenario reduction
+"""
+    mutable struct MeanValueReducer <: AbstractScenariosReducer
+    end
+
+A mutable structure representing an Expected Value (Mean Value) scenario reduction. Inherits from the `AbstractScenariosReducer` abstract type.
+
+# Constructor
+
+    MeanValueReducer() = new()
+
+"""
 mutable struct MeanValueReducer <: AbstractScenariosReducer
     MeanValueReducer() = new()
 end
 
+"""
+    function reduce(reducer::MeanValueReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
+
+Reduces the given `Scenarios` object using the specified `MeanValueReducer`, implementing the Expected Value scenario reduction method.
+
+# Arguments
+- `reducer::MeanValueReducer`: The reducer used to specify the reduction process.
+- `ω::Scenarios`: The `Scenarios` object to be reduced.
+
+# Keyword Arguments
+- `y::Int64 = 1`: The initial year to be considered.
+- `s::Int64 = 1`: The initial scenario to be considered.
+
+# Returns
+- A new reduced `Scenarios` object.
+- A probability vector representing the likelihood of each scenario (in this case, a single element array containing `1`).
+
+# Example
+
+```julia
+mean_value_reducer = MeanValueReducer()
+reduced_scenarios, probabilities = reduce(mean_value_reducer, scenarios)
+```
+"""
 function reduce(reducer::MeanValueReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
     # Mean value
     # Initialize
@@ -113,7 +222,28 @@ function reduce(reducer::MeanValueReducer, ω::Scenarios; y::Int64 = 1, s::Int64
     return Scenarios(demands, generations, storages, converters, grids), [1.]
 end
 
-# Clustering scenario reduction
+"""
+    mutable struct FeatureBasedReducer <: AbstractScenariosReducer
+        transformation::Union{UnionAll, Nothing}
+        reduction::Union{AbstractDimensionReducer, Nothing}
+        clustering::AbstractClusteringMethod
+    end
+
+A mutable structure representing a Clustering scenario reduction using feature-based methods. Inherits from the `AbstractScenariosReducer` abstract type.
+
+# Fields
+- `transformation`: A transformation method to apply before clustering (default: `UnitRangeTransform`)
+- `reduction`: A dimensionality reduction method to apply before clustering (default: `StatsReduction()`)
+- `clustering`: The clustering method to use (default: `KmedoidsClustering()`)
+
+# Constructor
+
+    FeatureBasedReducer(; transformation = UnitRangeTransform,
+                        reduction = StatsReduction(),
+                        clustering = KmedoidsClustering()) = new(transformation, reduction, clustering)
+
+
+"""
 mutable struct FeatureBasedReducer <: AbstractScenariosReducer
     transformation::Union{UnionAll, Nothing}
     reduction::Union{AbstractDimensionReducer, Nothing}
@@ -124,6 +254,33 @@ mutable struct FeatureBasedReducer <: AbstractScenariosReducer
                         clustering = KmedoidsClustering()) = new(transformation, reduction, clustering)
 end
 
+"""
+    function reduce(reducer::FeatureBasedReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
+
+Reduces the given `Scenarios` object using the specified `FeatureBasedReducer`, implementing a clustering-based scenario reduction method.
+
+# Arguments
+- `reducer::FeatureBasedReducer`: The reducer used to specify the reduction process, including transformation, dimension reduction, and clustering methods.
+- `ω::Scenarios`: The `Scenarios` object to be reduced.
+
+# Keyword Arguments
+- `y::Int64 = 1`: The initial year to be considered.
+- `s::Int64 = 1`: The initial scenario to be considered.
+
+# Returns
+- A new reduced `Scenarios` object.
+- A probability vector representing the likelihood of each scenario in the reduced set.
+- An array of assignments indicating which cluster each original scenario belongs to.
+
+# Example
+
+```julia
+feature_based_reducer = FeatureBasedReducer(transformation = UnitRangeTransform,
+                                            reduction = StatsReduction(),
+                                            clustering = KmedoidsClustering())
+reduced_scenarios, probabilities, assignments = reduce(feature_based_reducer, scenarios)
+```
+"""
 function reduce(reducer::FeatureBasedReducer, ω::Scenarios; y::Int64 = 1, s::Int64 = 1)
     # Parameters
     nh, ny, ns = size(ω.demands[1].power)
@@ -165,14 +322,50 @@ end
 # Transformation
 StatsBase.standardize(DT::Nothing, X; dims=nothing, kwargs...) = X
 
-# Dimension reduction
-# PCA
+"""
+    mutable struct PCAReduction <: AbstractDimensionReducer
+        n_components::Int64
+    end
+
+A mutable structure representing Principal Component Analysis (PCA) dimensionality reduction. Inherits from the `AbstractDimensionReducer` abstract type.
+
+# Fields
+- `n_components`: The number of principal components to use for dimensionality reduction (default: 2)
+
+# Constructor
+
+    PCAReduction(; n_components = 2) = new(n_components)
+
+"""
 mutable struct PCAReduction <: AbstractDimensionReducer
     n_components::Int64
 
     PCAReduction(; n_components = 2) = new(n_components)
 end
 
+
+"""
+    function dimension_reduction(reducer::PCAReduction, data::Array{Array{Float64,2}}; aggregated::Bool=false)
+
+Performs dimension reduction using PCA (Principal Component Analysis) on the input data.
+
+# Arguments
+- `reducer::PCAReduction`: The PCA reduction instance specifying the number of components to be used for reduction.
+- `data::Array{Array{Float64,2}}`: A vector of `d x n` matrices with `d` dimensions and `n` observations.
+
+# Keyword Arguments
+- `aggregated::Bool=false`: If `true`, PCA is applied on the aggregated data, otherwise, it is applied individually on each matrix in `data`.
+
+# Returns
+- The reduced data after applying PCA.
+
+# Example
+
+```julia
+pca_reduction = PCAReduction(n_components = 3)
+reduced_data = dimension_reduction(pca_reduction, data)
+```
+"""
 function dimension_reduction(reducer::PCAReduction, data::Array{Array{Float64,2}}; aggregated::Bool=false)
     # data is a vector of d x n matrix with d dimension and n observation
     if aggregated
@@ -184,9 +377,39 @@ function dimension_reduction(reducer::PCAReduction, data::Array{Array{Float64,2}
     end
 end
 
-# Statistical moments
+"""
+    struct StatsReduction <: AbstractDimensionReducer
+
+A structure representing statistics-based dimensionality reduction. Inherits from the `AbstractDimensionReducer` abstract type.
+
+# Constructor
+
+    StatsReduction()
+"""
 struct StatsReduction <: AbstractDimensionReducer end
 
+"""
+    function dimension_reduction(reducer::StatsReduction, data::Array{Array{Float64,2}}; aggregated::Bool=false)
+
+Performs dimension reduction using descriptive statistics on the input data.
+
+# Arguments
+- `reducer::StatsReduction`: The StatsReduction instance.
+- `data::Array{Array{Float64,2}}`: A vector of `d x n` matrices with `d` dimensions and `n` observations.
+
+# Keyword Arguments
+- `aggregated::Bool=false`: If `true`, calculates the statistics on the aggregated data, otherwise, calculates them individually for each matrix in `data`.
+
+# Returns
+- The reduced data containing sum, max, mean, variance, kurtosis, and skewness for each matrix in `data`.
+
+# Example
+
+```julia
+stats_reduction = StatsReduction()
+reduced_data = dimension_reduction(stats_reduction, data)
+```
+"""
 function dimension_reduction(reducer::StatsReduction, data::Array{Array{Float64,2}}; aggregated::Bool=false)
     # data is a vector of d x n matrix with d dimension and n observation
     if aggregated
@@ -219,8 +442,21 @@ end
 # No reduction
 dimension_reduction(reducer::Nothing, data::Array{Array{Float64,2}}) = vcat(data...)
 
-# Clustering methods
-# K-medoids
+"""
+    mutable struct KmedoidsClustering <: AbstractClusteringMethod
+
+A structure representing K-medoids clustering method. Inherits from the `AbstractClusteringMethod` abstract type.
+
+# Fields
+- `n_clusters::Int64`: Number of clusters to form.
+- `distance::Distances.SemiMetric`: Distance metric to use for calculating the dissimilarity between points.
+- `log::Bool`: Whether to print logs during the clustering process.
+
+# Constructor
+
+    KmedoidsClustering(; n_clusters = 20, distance = Distances.Euclidean(), log = true)
+
+"""
 mutable struct KmedoidsClustering <: AbstractClusteringMethod
     n_clusters::Int64
     distance::Distances.SemiMetric
@@ -229,6 +465,28 @@ mutable struct KmedoidsClustering <: AbstractClusteringMethod
     KmedoidsClustering(; n_clusters = 20, distance = Distances.Euclidean(), log = true) = new(n_clusters, distance, log)
 end
 
+
+"""
+    function clustering(method::KmedoidsClustering, embedding::AbstractArray{Float64,2})
+
+Performs K-medoids clustering on the given embedding.
+
+# Arguments
+- `method::KmedoidsClustering`: The KmedoidsClustering instance.
+- `embedding::AbstractArray{Float64,2}`: A `d x n` matrix with `d` dimensions and `n` observations.
+
+# Returns
+- `medoids`: The indices of the medoids.
+- `counts`: The number of elements in each cluster.
+- `assignments`: The cluster assignments for each observation.
+
+# Example
+
+```julia
+kmedoids_clustering = KmedoidsClustering(n_clusters=5, distance=Euclidean())
+medoids, counts, assignments = clustering(kmedoids_clustering, embedding)
+```
+"""
 function clustering(method::KmedoidsClustering, embedding::AbstractArray{Float64,2})
     # data is a d x n matrix with d dimension and n observation
     # Distance matrix
