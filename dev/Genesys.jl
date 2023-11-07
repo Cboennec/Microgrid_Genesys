dcc685e0119e0d0d84fc8e2e4a324d8aa177ab2a
Documenter.jl <documenter@juliadocs.github.io>
2023-11-07 12:34:41 +0000

    build based on 40cdfc8

--- dev/Genesys.jl
+++ dev/Genesys.jl
@@ -18,6 +18,8 @@ abstract type AbstractConverter end
 abstract type AbstractGrid end
 
 abstract type AbstractDesigner end
+abstract type AbstractDesignerNonFloat end
+
 abstract type AbstractController end
 
 
@@ -44,7 +46,7 @@ include(joinpath("assets","heater.jl"))
 include(joinpath("assets","grid.jl"))
 include(joinpath("assets","solar.jl"))
 include(joinpath("assets","demand.jl"))
-export AbstractController, AbstractLiion, AbstractDesigner
+export AbstractController, AbstractLiion, AbstractDesigner, AbstractDesignerNonFloat
 export Microgrid, Demand, Solar, Liion_energy_exchanged, Liion_rainflow, Liion_fixed_lifetime, Liion_vermeer, Liion_electro_chimique, Tremblay_dessaint_params, vermeer_params, Electro_chimique_params, ThermalStorage, H2Tank, FuelCell, Electrolyzer, Heater, Grid, GlobalParameters
 export EnergyCarrier, Electricity, Heat, Hydrogen
 export add!
